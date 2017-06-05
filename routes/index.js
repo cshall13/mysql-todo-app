044c16e4066ad1699383699f6ef9bca465e1d9f2
Shane Hall <cyrus.s.hall84@gmail.com>
2017-06-05 16:05:23 -0400

    added some database functionality

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,46 @@ var connection = mysql.createConnection({
 	database: config.database
 });
 
+
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var message = req.query.msg;
+	if(message == "added"){
+		message = 'Your task was added!';
+	}
+	var selectQuery = "SELECT * FROM tasks";
+	connection.query(selectQuery, (error,results)=>{
+  		res.render('index', { 
+  			message: message,
+  			taskArray: results
+  		});
+	});
 });
 
 // add a post route "addItem" to handle the form submission
 router.post('/addItem',(req,res)=>{
-	res.json(req.body);
+	// res.json(req.body);
 	var newTask = req.body.newTask;
 	var dueDate = req.body.newTaskDate;
 	// we know what they submitted from the form. it comes to this route 
-	// inside req.body.
+	// inside req.body.NAMEOFFIELD. Now we need to insert it into MySql.
+	var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES ('"+newTask+"','"+dueDate+"')";
+	// res.send(insertQuery);
+	connection.query(insertQuery, (error,results)=>{
+		if(error) {throw error};
+		res.redirect('/?msg=added');
+	});
+});
+
+router.get('/delete/:id', (req,res)=>{
+	var idToDelete = req.params.id;
+	var deleteQuery = "DELETE from stasks WHERE id = " + idToDelete;
+	connection.query(deleteQuery,(error,results)=>{
+		res.redirect('/?msg=deleted')
+	});
+	// res.send(idToDelete);
 });
 
 module.exports = router;
